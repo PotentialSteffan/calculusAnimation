904fd217712f5029f8643ee6c234cbb33e7aa09f
李治禹 <lizhiyu@920ly.cn>
2021-06-08 18:01:10 +0800

    part leave

--- assets/src/ArcComp.ts
+++ assets/src/ArcComp.ts
@@ -1,20 +1,27 @@
-const {ccclass, property} = cc._decorator;
+const { ccclass, property } = cc._decorator;
 
 @ccclass
 export default class ArcComp extends cc.Component {
-    onLoad(){
+    onLoad() {
         const _graphic = this.node.getComponent(cc.Graphics);
-        _graphic.moveTo(0,0);
-        _graphic.lineTo(100,0);
-        _graphic.arc(0,0,100,0,Math.PI/5,true);
-        _graphic.lineTo(0,0);
+        _graphic.moveTo(0, 0);
+        let _r = 100;
+        let _halfArc = Math.PI / 5 / 2;
+        let _beginX = _r * Math.cos(_halfArc);
+        let _beginY = -_r * Math.sin(_halfArc);
+        _graphic.lineTo(_beginX, _beginY);
+        _graphic.arc(0, 0, 100,-_halfArc, _halfArc, true);
+        _graphic.lineTo(0, 0);
         _graphic.stroke();
     }
 
-    start () {
+    start() {
     }
 
-    update(){
-
+    update() {
+        // if(this.node.angle == 90){
+        //     this.node.parent.parent.addChild(this.node);
+        // }
+        // cc.log(this.node.angle)
     }
 }
--- assets/src/Main.ts
+++ assets/src/Main.ts
@@ -1,4 +1,4 @@
-const {ccclass, property} = cc._decorator;
+const { ccclass, property } = cc._decorator;
 
 @ccclass
 export default class Main extends cc.Component {
@@ -8,17 +8,54 @@ export default class Main extends cc.Component {
     @property(cc.Prefab)
     arcPrefab: cc.Prefab = null;
 
-    totalCount:number = 10;
-    start () {
+    totalCount: number = 10;
+    start() {
         for (let i = 0; i < this.totalCount; i++) {
             let _arcIns = cc.instantiate(this.arcPrefab);
-            this.arcContainer.addChild(_arcIns); 
+            this.arcContainer.addChild(_arcIns);
         }
-        
+
         for (let i = 0; i < this.arcContainer.childrenCount; i++) {
-            const _arc:cc.Node = this.arcContainer.children[i];
-            let _rotateArc = i * Math.PI/this.totalCount*360/Math.PI;
-            new cc.Tween(_arc).to(1,{rotation:-_rotateArc}).start(); 
+            const _arc: cc.Node = this.arcContainer.children[i];
+            let _rotateArc = i * 360 / this.totalCount;
+            new cc.Tween(_arc).to(1, { angle: -_rotateArc })
+                .call(() => {
+                    if (i == this.arcContainer.childrenCount - 1) {
+                        this.arcContainerExpand();
+                    }
+                })
+                .start()
         }
+    }
+
+    arcContainerExpand() {
+        let _that = this;
+        // new cc.Tween(this.arcContainer).parallel(
+        new cc.Tween(this.arcContainer).to(1, { x: 100 }).call(() => {
+            let _item = _that.arcContainer.children[0];
+            let _localPos = _item.position;
+            let _pos = _that.arcContainer.convertToWorldSpaceAR(_localPos);
+            _item.parent = _that.arcContainer.parent;
+            _item.position = _pos;
+        }).then(
+            new cc.Tween(this.arcContainer).to(3, { x: 200 }).call(() => {
+
+            })
+        ).start()
+
+        // new cc.Tween(this.arcContainer).to(1, { angle: 360 / _that.totalCount }).call(() => {
+        //     cc.log("angle change end" + new Date().getTime())
+        //     let _item = _that.arcContainer.children[0];
+        //     let _localPos = _item.position;
+        //     let _pos = _that.arcContainer.convertToWorldSpaceAR(_localPos);
+        //     _item.parent = _that.arcContainer.parent;
+        //     _item.position = new cc.Vec3(0, 0, 0);
+        // })
+        // new cc.Tween().to(3, { angle: 360 }),
+        // ).start();
+    }
+
+    update() {
+
     }
 }
